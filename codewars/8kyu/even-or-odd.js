c24ad168666036240e7ab915f3c0a6e2b778d13e
Steve Magana <steveamagana@gmail.com>
2023-01-31 00:41:58 -0800

    Add new 8kyu codewars challenge

---
+++ codewars/8kyu/even-or-odd.js
@@ -0,0 +1,14 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// My solutions:
+function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+}
+
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+}
