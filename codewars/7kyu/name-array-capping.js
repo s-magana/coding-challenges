03de5710521be41ca81e4d8208a5556a91876c36
Steve Magana <steveamagana@gmail.com>
2023-04-01 23:15:53 -0700

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/name-array-capping.js
@@ -0,0 +1,11 @@
+// Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+// example
+
+// capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+// capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+
+// My solution:
+function capMe(names) {
+    return names.map(e => e.slice(0,1).toUpperCase() + e.slice(1).toLowerCase())
+}
