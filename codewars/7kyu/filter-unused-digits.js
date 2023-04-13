25bb14c12edaeb3a6b4679201167bb293be97e35
Steve Magana <steveamagana@gmail.com>
2023-04-13 02:47:59 -0700

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/filter-unused-digits.js
@@ -0,0 +1,16 @@
+// Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+// Example:
+
+// [12, 34, 56, 78]  =>  "09"
+// [2015, 8, 26]     =>  "3479"
+// Note: the digits in the resulting string should be sorted.
+
+// My solution:
+function unusedDigits(...arr) {
+    let digits = arr.join()
+    
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+      .filter(x => !digits.includes(x))
+      .join('');
+}
