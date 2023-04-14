af7bd399df138be63b48baa569e28a00984b1382
Steve Magana <steveamagana@gmail.com>
2023-04-14 02:49:02 -0700

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/alphabet-war.js
@@ -0,0 +1,62 @@
+// Introduction
+// There is a war and nobody knows - the alphabet war!
+// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+
+// Task
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+// The other letters don't have power and are only victims.
+
+// Example
+// alphabetWar("z");        //=> Right side wins!
+// alphabetWar("zdqmwpbs"); //=> Let's fight again!
+// alphabetWar("zzzzs");    //=> Right side wins!
+// alphabetWar("wwwwwwz");  //=> Left side wins!
+
+// My solution:
+function alphabetWar(fight) {
+    const left_side = {
+      'w': 4,
+      'p': 3,
+      'b': 2,
+      's': 1
+    }
+    
+    const right_side = {
+      'm': 4,
+      'q': 3,
+      'd': 2,
+      'z': 1
+    }
+    
+    let left = 0
+    let right = 0
+    
+    for (let char of fight) {
+      if (char in left_side) {
+        left += left_side[char]
+      } else if (char in right_side) {
+        right += right_side[char]
+      }
+    }
+    
+    if (right > left) {
+      return 'Right side wins!'
+    } else if (left > right) {
+      return 'Left side wins!'
+    } else {
+      return "Let's fight again!"
+    }
+}
