c406ebd644a89ffda094af2b6e74aa0663c9da75
Steve Magana <steveamagana@gmail.com>
2023-08-11 03:24:40 -0700

    Add new 8kyu codewars challenge

---
+++ codewars/8kyu/grasshopper-messi-goals-function.js
@@ -0,0 +1,20 @@
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+// My solutions: 
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
+
+const goals = (...a) => a.reduce((acc, c) => acc + c, 0);
