298cc559d23af35e4b7bd477a2f6d55e8b047101
Steve Magana <steveamagana@gmail.com>
2023-08-22 03:27:09 -0700

    Add new 8kyu codewars challenge

---
+++ codewars/8kyu/rock-paper-scissors.js
@@ -0,0 +1,51 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// My solutions: 
+var rps = (p1, p2) => {
+    if (p1 === 'rock' && p2 === 'paper') {
+      return 'Player 2 won!'
+    } else if (p1 === 'scissors' && p2 === 'paper') {
+      return 'Player 1 won!'
+    } else if (p1 === 'rock' && p2 === 'scissors') {
+      return 'Player 1 won!'
+    } else if (p1 === 'scissors' && p2 === 'rock') {
+      return 'Player 2 won!'
+    } else if (p1 === 'paper' && p2 === 'rock') {
+      return 'Player 1 won!'
+    } else if (p1 === 'paper' && p2 === 'scissors') {
+      return 'Player 2 won!'
+    } else if (p1 === p2) {
+      return 'Draw!'
+    }
+};
+
+var rps = (p1, p2) => {
+    if (p1 === p2) return "Draw!";
+    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+    if (p2 === rules[p1]) {
+      return "Player 1 won!";
+    }
+    else {
+      return "Player 2 won!";
+    }
+};
+
+var rps = (p1, p2) => {
+    if (p1 == p2)
+      return 'Draw!';
+      
+     if (p1 == 'rock' && p2 == 'scissors') 
+       return 'Player 1 won!'
+     else if (p1 == 'scissors' && p2 == 'paper') 
+       return 'Player 1 won!'
+     else if (p1 == 'paper' && p2 == 'rock') 
+       return 'Player 1 won!'
+     else
+       return 'Player 2 won!';
+};
