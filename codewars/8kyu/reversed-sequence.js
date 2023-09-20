c4ab758631e5af19e4983d7fcd59dbe684f97ba4
Steve Magana <steveamagana@gmail.com>
2023-09-20 02:46:45 -0700

    Add new 8kyu codewars challenge

---
+++ codewars/8kyu/reversed-sequence.js
@@ -0,0 +1,23 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+// My solutions:
+const reverseSequ = n => {
+    let arr = []
+    for (let i = 1; i <= n; i++) {
+      arr.unshift(i)
+    }
+    return arr
+};
+
+const reverseSeque = n => {
+    let arr = [];
+      for (let i=n; i>0; i--) {
+        arr.push(i);
+        } return arr;
+};
+
+const reverseSeq = n => {
+    return Array(n).fill(0).map((e, i) => n - i );
+};
