ca7f538b98153a225656b02c6882317c4480fc8e
Steve Magana <steveamagana@gmail.com>
2023-10-24 03:12:36 -0700

    Add new 8kyu codewars challenge

---
+++ codewars/8kyu/5-without-numbers.js
@@ -0,0 +1,15 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+// My solutions: 
+function unusualFive() {
+    let five = 'five!'
+    return five.length
+}
+
+function unusualFive() {
+    return 'kirby'.length
+}
