89e80ceebd72a7d3f118c77c4224899a1f6ef2fa
Steve Magana <steveamagana@gmail.com>
2023-12-02 01:28:15 -0800

    Add new 8kyu codewars challenge

---
+++ codewars/8kyu/type-of-sum.js
@@ -0,0 +1,9 @@
+// Return the type of the sum of the two arguments
+
+// My solutions: 
+function typeOfSum(a, b) {
+    let sum = a + b
+    return typeof sum
+}
+
+const typeOfSum = (a, b) => typeof(a + b)
