c0fc9868e20c42c29a88645b773d060ac4f073bb
Steve Magana <steveamagana@gmail.com>
2024-03-14 01:13:13 -0700

    Add new 6kyu codewars challenge

---
+++ codewars/6kyu/string-transformer.js
@@ -0,0 +1,15 @@
+// Given a string, return a new string that has transformed based on the input:
+
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+// You may assume the input only contain English alphabet and spaces.
+
+// My solution: 
+function stringTransformer(str) {
+    return str.split(' ').map(e => e.split('').map(f => f === f.toLowerCase() ? f.toUpperCase() : f.toLowerCase()).join('')).reverse().join(' ')
+}
