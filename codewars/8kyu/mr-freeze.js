92c93465b4e32de80215936b5dbecf4f8c9d7291
Steve Magana <steveamagana@gmail.com>
2024-04-09 01:26:45 -0700

    Add new 8kyu codewars challenge

---
+++ codewars/8kyu/mr-freeze.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+// My solution: 
+Object.freeze(MrFreeze)
