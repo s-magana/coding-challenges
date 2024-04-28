0899fb76b12b90bea4aca81d8da131e27c6fd59d
Steve Magana <steveamagana@gmail.com>
2024-04-28 01:49:35 -0700

    Add new 8kyu codewars challenge

---
+++ codewars/8kyu/ascii-total.js
@@ -0,0 +1,11 @@
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+
+// Examples:
+
+// uniTotal("a") == 97
+// uniTotal("aaa") == 291
+
+// My solution:
+function uniTotal (string) {
+    return string.split('').reduce((acc, c) => acc + c.charCodeAt(), 0)
+}
