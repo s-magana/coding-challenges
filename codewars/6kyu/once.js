ffb58cbbb2e381213793f5712b78f30cd1ace688
Steve Magana <steveamagana@gmail.com>
2024-07-15 23:34:13 -0700

    Add new 6kyu codewars challenge

---
+++ codewars/6kyu/once.js
@@ -0,0 +1,20 @@
+// You'll implement once, a function that takes another function as an argument, and returns a new version of that function that can only be called once.
+
+// Subsequent calls to the resulting function should have no effect (and should return undefined).
+
+// For example:
+
+// logOnce = once(console.log)
+// logOnce("foo") // -> "foo"
+// logOnce("bar") // -> no effect
+
+// My solution: 
+function once(fn) {
+    let called = false 
+    return function() { 
+          if (!called) { 
+              called = true
+              return fn.apply(this, arguments)
+          }
+      }
+}
