73f14c52d6808ac4c2f1d646b0ba4ef54a3dd436
Steve Magana <steveamagana@gmail.com>
2024-12-26 01:28:48 -0800

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/words-to-sentence.js
@@ -0,0 +1,10 @@
+// Write function which will create a string from a list of strings, separated by space.
+
+// Example:
+
+// ["hello", "world"] -> "hello world"
+
+// My solution: 
+function wordsToSentence(words) {
+    return words.join(' ')
+}
