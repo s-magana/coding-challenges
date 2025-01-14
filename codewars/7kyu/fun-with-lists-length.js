d6212f4dcd6d95eac4c282a1936055fdda8c7745
Steve Magana <steveamagana@gmail.com>
2025-01-14 01:13:39 -0800

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/fun-with-lists-length.js
@@ -0,0 +1,18 @@
+// Implement the method length, which accepts a linked list (head), and returns the length of the list.
+
+// For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+
+// The linked list is defined as follows:
+
+// function Node(data, next = null) {
+//   this.data = data;
+//   this.next = next;
+// }
+// Note: the list may be null and can hold any type of value.
+
+// Good luck!
+
+// My solution: 
+function length(head) {
+    return head ? 1 + length(head.next) : 0
+}
