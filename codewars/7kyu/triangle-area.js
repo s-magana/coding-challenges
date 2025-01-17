b51a81b6e4f1900984e50e4d51c37bf80f13ae47
Steve Magana <steveamagana@gmail.com>
2025-01-17 01:47:50 -0800

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/triangle-area.js
@@ -0,0 +1,26 @@
+// Task.
+
+// Calculate area of given triangle. Create a function t_area that will take a string which will represent triangle, find area of the triangle, one space will be equal to one length unit. The smallest triangle will have one length unit.
+
+// Hints
+
+// Ignore dots.
+
+// All triangles will be right isoceles.
+
+// Example:
+
+// .
+// .      .  
+// .      .       .      ---> should return 2.0
+
+// .
+// .      .  
+// .      .       .     
+// .      .       .      .      ---> should return 4.5
+
+// My solution: 
+function tArea(tStr) {
+    let side = tStr.split('\n').length - 3
+    return (side * side) / 2
+}
