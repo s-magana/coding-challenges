79919c385c041754ea29f97034213b6974b8b40a
Steve Magana <steveamagana@gmail.com>
2025-02-23 01:32:39 -0800

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/replace-all-items.js
@@ -0,0 +1,12 @@
+// Write function replaceAll (Python: replace_all) that will replace all occurrences of an item with another.
+
+// Python / JavaScript: The function has to work for strings and lists.
+
+// Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
+
+// replaceAll(replaceAll(array: [1,2,2], old: 1, new: 2) // [2,2,2]
+
+// My solution: 
+function replaceAll(seq, find, replace) {
+    return typeof seq === 'string' ? seq.replaceAll(find, replace): seq.map(e => e === find ? replace : e)
+}
