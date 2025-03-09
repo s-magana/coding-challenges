c8bc3494ed05adf81ada03e321f264dbfbcc088e
Steve Magana <steveamagana@gmail.com>
2025-03-09 02:20:00 -0700

    Add new hackerank challenge

---
+++ hackerrank/lonely-integer.js
@@ -0,0 +1,39 @@
+// Given an array of integers, where all elements but one occur twice, find the unique element.
+
+// Example
+
+// The unique element is .
+
+// Function Description
+
+// Complete the lonelyinteger function in the editor below.
+
+// lonelyinteger has the following parameter(s):
+
+// int a[n]: an array of integers
+// Returns
+
+// int: the element that occurs only once
+// Input Format
+
+// The first line contains a single integer, , the number of integers in the array.
+// The second line contains  space-separated integers that describe the values in .
+
+// Constraints
+
+// It is guaranteed that  is an odd number and that there is one unique element.
+// , where .
+
+// Solution:
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    // Write your code here
+    return a.find(e => a.indexOf(e) === a.lastIndexOf(e))
+
+}
