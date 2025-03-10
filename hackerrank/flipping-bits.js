b76a2159f2ae2dc6aeaedb5ed2eea0882154d307
Steve Magana <steveamagana@gmail.com>
2025-03-10 02:20:33 -0700

    Add new hackerank challenge

---
+++ hackerrank/flipping-bits.js
@@ -0,0 +1,58 @@
+// You will be given a list of 32 bit unsigned integers. Flip all the bits ( and ) and return the result as an unsigned integer.
+
+// Example
+
+// . We're working with 32 bits, so:
+
+
+
+// Return .
+
+// Function Description
+
+// Complete the flippingBits function in the editor below.
+
+// flippingBits has the following parameter(s):
+
+// int n: an integer
+// Returns
+
+// int: the unsigned decimal integer result
+// Input Format
+
+// The first line of the input contains , the number of queries.
+// Each of the next  lines contain an integer, , to process.
+
+// Constraints
+
+
+
+// Sample Input
+
+// 3 
+// 2147483647 
+// 1 
+// 0
+// Sample Output
+
+// 2147483648 
+// 4294967294 
+// 4294967295
+// Explanation
+
+// Take 1 for example, as unsigned 32-bits is 00000000000000000000000000000001 and doing the flipping we get 11111111111111111111111111111110 which in turn is 4294967294.
+
+// Solution: 
+/*
+ * Complete the 'flippingBits' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+function flippingBits(n) {
+    // Write your code here
+    let bits = n.toString(2).padStart(32, '0')
+    let flipped = bits.split('').map(e => e === '0' ? '1' : '0').join('')
+    return parseInt(flipped, 2)
+}
