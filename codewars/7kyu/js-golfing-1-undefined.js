fd5081959b12f9f89d8070884b2ab6359713ea17
Steve Magana <steveamagana@gmail.com>
2025-06-07 13:37:27 -0700

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/js-golfing-1-undefined.js
@@ -0,0 +1,11 @@
+//  JS Golfing Series 
+
+// Task:
+
+// Return undefined with no more than 10 characters.
+
+// Digits and letters are not allowed!
+// Your function should take exactly `0` parameters.
+
+// My solution:
+x=()=>{}
