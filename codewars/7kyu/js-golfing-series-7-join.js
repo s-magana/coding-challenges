bb5f1c64559e0b87ceaec4e24f3ee0bc14ca0667
Steve Magana <steveamagana@gmail.com>
2025-06-24 13:43:52 -0700

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/js-golfing-series-7-join.js
@@ -0,0 +1,10 @@
+// JS Golfing Series 
+
+// Task:
+
+// Return an array as string with no more than 13 characters. The delimiter should be an empty string, e.g. [123, "456"] should be stringified into "123456"
+
+// Note that: undefined and null are shown as empty string.
+
+// My solution:
+x=a=>a.join``
