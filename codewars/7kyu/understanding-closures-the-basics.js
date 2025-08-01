99d37ea83e69ef6bd098c7ede261493d9f951cf7
Steve Magana <steveamagana@gmail.com>
2025-08-01 14:13:36 -0700

    Add new 7kyu codewars challenge

---
+++ codewars/7kyu/understanding-closures-the-basics.js
@@ -0,0 +1,14 @@
+// This is a simple exercise to understand the feature in the javascript language called closure.
+
+// The function buildFun will return an array of functions. The single parameter accepted by buildFun is the number of elements N of the array returned.
+
+// The wanted outcome is that when all function in the array are executed, the number from 0 to N should be returned.
+
+// My solution: 
+function buildFun(n){
+	let res = []
+	for (let i = 0; i < n; i++){
+		res.push(() => i)
+	}
+	return res
+}
